b19e6db1f72daa6103991dc8f7e536e008160288
42045764 <rathhen123@gmail.com>
2019-10-25 21:45:24 -0400

    Updates

---
+++ precache-manifest.fc9cfe5185926801291c05c94986f5eb.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "56d408a2aa85415f1a285d1219f02498",
+    "url": "/FinancialGuru/index.html"
+  },
+  {
+    "revision": "7f8ba377fd08cdf16ef5",
+    "url": "/FinancialGuru/static/js/2.11a848c8.chunk.js"
+  },
+  {
+    "revision": "cf5905709ebc00c08e2a",
+    "url": "/FinancialGuru/static/js/main.0f5b685c.chunk.js"
+  },
+  {
+    "revision": "eb98d1365a38bfa42e28",
+    "url": "/FinancialGuru/static/js/runtime~main.eb2a8122.js"
+  }
+]);
